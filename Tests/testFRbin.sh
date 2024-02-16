e4207b961d77bdfe01b5339e3524b6fc02f02098
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-02-16 15:26:54 +0000

    more testing in a container

--- Tests/testFRbin.sh
+++ Tests/testFRbin.sh
@@ -11,6 +11,7 @@ if [[ "$hn" == "testpi4" || "$hn" == "testpi5" ]] ; then
     pip install -r requirements.txt
     python setup.py install
     CFG=$RMSDIR/.config
+    DATA=$here/frbintests
 elif [ "$hn" == "MARKSDT" ] ; then 
     RMSDIR=/mnt/e/dev/meteorhunting/RMS
     source ~/.bashrc
@@ -18,44 +19,52 @@ elif [ "$hn" == "MARKSDT" ] ; then
     cd $RMSDIR
     pip install -r requirements.txt
     CFG=$here/frbintests/.config
+    DATA=$here/frbintests
 else
-    echo testing not implemented on this platform  $(uname -a) $hn yet
-    exit 1
+    RMSDIR=$(pwd)
+    source ~/.bashrc
+    conda activate $HOME/miniconda3/envs/RMS
+    cd $RMSDIR
+    pip install -r requirements.txt
+    CFG=/data/testing/frbintests/.config
+    DATA=/data/testing/frbintests
 fi
-mkdir -p $here/frbintests/results
-rm $here/frbintests/results/FR*.mp4
-python -m Utils.FRbinViewer $here/frbintests/ -x -f mp4 
+pwd 
+echo $DATA $RMSDIR
+mkdir -p $DATA/results
+rm $DATA/results/FR*.mp4
+python -m Utils.FRbinViewer $DATA/ -x -f mp4 
 # basic video production
-ls -1 $here/frbintests/FR*.mp4 | while read i ; do
-    mv -f $i $here/frbintests/results/$(basename $i)-noextras.mp4
+ls -1 $DATA/FR*.mp4 | while read i ; do
+    mv -f $i $DATA/results/$(basename $i)-noextras.mp4
 done
 # with a config file but no other options - should be the same as basic
-python -m Utils.FRbinViewer $here/frbintests/ -x -f mp4 -c $CFG
-ls -1 $here/frbintests/FR*.mp4 | while read i ; do
-    mv -f $i $here/frbintests/results/$(basename $i)-withcfg.mp4
+python -m Utils.FRbinViewer $DATA/ -x -f mp4 -c $CFG
+ls -1 $DATA/FR*.mp4 | while read i ; do
+    mv -f $i $DATA/results/$(basename $i)-withcfg.mp4
 done
 # add shower name to the video but not the still
-python -m Utils.FRbinViewer $here/frbintests/ -x -f mp4 -w -c $CFG
-ls -1 $here/frbintests/FR*.mp4 | while read i ; do
-    mv -f $i $here/frbintests/results/$(basename $i)-shwronly.mp4
+python -m Utils.FRbinViewer $DATA/ -x -f mp4 -w -c $CFG
+ls -1 $DATA/FR*.mp4 | while read i ; do
+    mv -f $i $DATA/results/$(basename $i)-shwronly.mp4
 done
 # add still  to the video but not the shower name
-python -m Utils.FRbinViewer $here/frbintests/ -x -f mp4 -m -c $CFG
-ls -1 $here/frbintests/FR*.mp4 | while read i ; do
-    mv -f $i $here/frbintests/results/$(basename $i)-ffonly.mp4
+python -m Utils.FRbinViewer $DATA/ -x -f mp4 -m -c $CFG
+ls -1 $DATA/FR*.mp4 | while read i ; do
+    mv -f $i $DATA/results/$(basename $i)-ffonly.mp4
 done
 # add timestamp to the video but not the shower name
-python -m Utils.FRbinViewer $here/frbintests/ -x -f mp4 -t -c $CFG
-ls -1 $here/frbintests/FR*.mp4 | while read i ; do
-    mv -f $i $here/frbintests/results/$(basename $i)-tsonly.mp4
+python -m Utils.FRbinViewer $DATA/ -x -f mp4 -t -c $CFG
+ls -1 $DATA/FR*.mp4 | while read i ; do
+    mv -f $i $DATA/results/$(basename $i)-tsonly.mp4
 done
 # add everything
-python -m Utils.FRbinViewer $here/frbintests/ -x -f mp4 -w -c $CFG -m -t
-ls -1 $here/frbintests/FR*.mp4 | while read i ; do
-    mv -f $i $here/frbintests/results/$(basename $i)-shwr_all.mp4
+python -m Utils.FRbinViewer $DATA/ -x -f mp4 -w -c $CFG -m -t
+ls -1 $DATA/FR*.mp4 | while read i ; do
+    mv -f $i $DATA/results/$(basename $i)-shwr_all.mp4
 done
 # add everything but accidentally forget the config file - should fail to add showerid
-python -m Utils.FRbinViewer $here/frbintests/ -x -f mp4 -w -m -t
-ls -1 $here/frbintests/FR*.mp4 | while read i ; do
-    mv -f $i $here/frbintests/results/$(basename $i)-shwr_ff_ts_nocfg.mp4
+python -m Utils.FRbinViewer $DATA/ -x -f mp4 -w -m -t
+ls -1 $DATA/FR*.mp4 | while read i ; do
+    mv -f $i $DATA/results/$(basename $i)-shwr_ff_ts_nocfg.mp4
 done
